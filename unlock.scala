41401e434f9a25e0fbb05b1504a43c056aec7ca0
Marko Elezovic <marko@mentat-labs.com>
2016-03-03 02:34:41 +0100

    Added debug output with x10 enlarged frames Added parallel image processing

--- unlock.scala
+++ unlock.scala
@@ -1,39 +1,62 @@
+import java.awt.{Color, Font, RenderingHints}
 import java.awt.image.BufferedImage
+import java.io.{File, FileInputStream}
+import java.util.concurrent.Executors
 import java.util.zip.ZipInputStream
-import java.io.FileInputStream
 import javax.imageio.ImageIO
+
+import scala.collection.immutable.VectorBuilder
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 val Width = 50
 val Height = 50
 
 val BaseAreaMinThreshold = 0.85
 val KeyAreaMaxThreshold = 0.002
+val KeyReplacementColor = 0
+
+// Job queue, we're using all CPU cores
+val jobs = new VectorBuilder[Future[Unit]]
+implicit val executionContext = ExecutionContext.fromExecutor(
+  Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors))
 
 // read frames from the archive to speed up reading time
 val zis = new ZipInputStream(new FileInputStream("input/♐LOCK-frames.zip"))
-readFrames(7200 to 7220)
+processFrames(7200 to 7220)
 zis.close()
+
+// Wait for jobs to complete
+Await.result(Future.sequence(jobs.result), Duration.Inf)
 sys.exit(0)
 
-def readFrames(range: Range): Unit = {
+def processFrames(range: Range): Unit = {
   val ze = zis.getNextEntry
   if (ze ne null) {
     val frameNumber = ze.getName.replace(".png", "")
     if (range.contains(frameNumber.toInt)) {
-      val id = "♐LOCK - frame #" + frameNumber
+      val id = "LOCK - frame #" + frameNumber
 
-      println(s"$id: Reading ...")
-      val frame = ImageIO.read(zis)
-      // sanity check on frame dimensions
-      require(frame.getWidth == Width && frame.getHeight == Height)
+      println(s">-- $id: Reading ...")
+      val original = ImageIO.read(zis)
 
-      println(s"$id: Processing frame ...")
-      val keyInfo = processFrame(frame)
-      println(s"$id: $keyInfo")
+      jobs += Future {
+        require(original.getWidth == Width)
+        require(original.getHeight == Height)
+
+        val frameInfo = findKey(original)
+        println(s"--> $id: Cleaning ...")
+        val cleaned = cleanFrame(original, frameInfo)
+
+        println(s"--< $id: Writing ...")
+        writeDebugFrame(id, original, frameInfo, cleaned)
+
+        println(s"<-- $id: Done!")
+      }
     }
 
     // recursion, no change in range
-    readFrames(range)
+    processFrames(range)
   }
 }
 
@@ -45,13 +68,17 @@ case class RGB private(r: Int, g: Int, b: Int) {
     val db = other.b - b
     dr * dr + dg * dg + db * db
   }
+
+  def rgb = (b << 16) + (g << 8) + r
 }
 
 object RGB {
   def apply(rgb: Int) = new RGB(rgb & 0xff, (rgb >>> 8) & 0xff, (rgb >>> 16) & 0xff)
 }
 
-def processFrame(frame: BufferedImage): (RGB, Option[(RGB, Int)]) = {
+case class FrameInfo(baseColor: RGB, key: Option[(RGB, Int)])
+
+def findKey(frame: BufferedImage): FrameInfo = {
   val pixels = frame.getRGB(0, 0, Width, Height, null, 0, Width)
   val colors = pixels.groupBy(RGB(_)).mapValues(_.size)
 
@@ -75,5 +102,59 @@ def processFrame(frame: BufferedImage): (RGB, Option[(RGB, Int)]) = {
       None
     }
 
-  baseColor -> key
+  FrameInfo(baseColor = baseColor, key = key)
+}
+
+def cleanFrame(original: BufferedImage, frameInfo: FrameInfo): BufferedImage = {
+  val pixels = original.getRGB(0, 0, Width, Height, null, 0, Width)
+
+  val fixedPixels = frameInfo.key match {
+    case Some((key, _)) =>
+      // in we have a key, place replacement color on top of those pixels, baseColor everything else
+      pixels map { p =>
+        if (RGB(p) == key) KeyReplacementColor else frameInfo.baseColor.rgb
+      }
+    case _ =>
+      // in case no key was detected, fill entire screen with baseColor
+      Array.fill(pixels.length)(frameInfo.baseColor.rgb)
+  }
+
+  val img = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_BGR)
+  img.setRGB(0, 0, Width, Height, fixedPixels, 0, Width)
+  img
+}
+
+// Some graphics for visual comparison
+def writeDebugFrame(id: String, original: BufferedImage, frameInfo: FrameInfo, cleaned: BufferedImage): Unit = {
+  val debugFrame = new BufferedImage(1200, 1000, BufferedImage.TYPE_INT_BGR)
+
+  val g2d = debugFrame.createGraphics()
+  g2d.setColor(Color.white)
+  g2d.setFont(new Font("Consolas", Font.PLAIN, 30))
+  g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+  g2d.drawString(id, 50, 50)
+
+  g2d.drawString("Original", 50, 130)
+  g2d.drawRect(49, 149, Width + 1, Height  + 1)
+  g2d.drawImage(original, 50, 150, null)
+
+  g2d.drawString("Original (x10)", 50, 270)
+  g2d.drawRect(49, 299, 501, 501)
+  g2d.drawImage(original, 50, 300, 550, 800, 0, 0, Width, Height, null)
+
+  g2d.drawString("Cleaned", 600, 130)
+  g2d.drawRect(599, 149, Width + 1, Height  + 1)
+  g2d.drawImage(cleaned, 600, 150, null)
+
+  g2d.drawString("Cleaned (x10)", 600, 270)
+  g2d.drawRect(599, 299, 501, 501)
+  g2d.drawImage(cleaned, 600, 300, 1100, 800, 0, 0, Width, Height, null)
+
+  g2d.drawString("Base color: #%06X" format frameInfo.baseColor.rgb, 50, 850)
+  val keyInfo = frameInfo.key.map(k => "Found keys: " + k._2).getOrElse("No keys found!")
+  g2d.drawString(keyInfo, 50, 890)
+
+  g2d.dispose()
+
+  ImageIO.write(debugFrame, "png", new File(s"output/$id.png"))
 }
